0bc9950b27e38d2903028c7b5f514c752ac3318d
veronika <sustv153@gmail.com>
2026-01-19 11:29:47 +0200

    snack

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,35 @@
-// Описаний у документації
 import iziToast from "izitoast";
-// Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
+const form = document.querySelector('.form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const delay = parseInt(event.currentTarget.elements.delay.value);
+    const state = event.currentTarget.elements.state.value;
+    createPro(delay, state)
+    .then((delay) => {
+        iziToast.success({
+            title: 'OK',
+            message: `Fulfilled promide in ${delay}ms`,
+            position: 'topRight'
+        });
+    })
+    .catch((delay) => {
+    iziToast.error({
+        title: 'Error',
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight'
+    });
+    });
+    form.reset();
+});
+function createPro(delay, state) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay)
+            }
+        }, delay);
+    });
+}
